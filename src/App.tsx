0bf6282cabd4eb900d4bc7122d4d437a33295484
Daniil <ralf.danya@mail.ru>
2022-07-30 18:59:33 +0700

    add interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Product from "./components/Product/Product";
+import { data } from '../src/data/data'
+import { ProductInterface } from "./Interfaces/Interfaces";
+
+
 
 function App() {
+
+  let construct = data.map((item: ProductInterface) => {
+    return <Product key={item.id} product={item}></Product>
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto max-w-2xl pt-5">
+      {construct}
     </div>
   );
 }
---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { ProductInterface } from "../../Interfaces/Interfaces";
+
+interface ProductProps{
+    product: ProductInterface;
+}
+
+
+
+function Product(props: ProductProps) {
+
+    let [btnClick, setBtnClick] = useState(false);
+    let click = () => {
+        if(btnClick === false){
+            setBtnClick(true);
+        } else{
+            setBtnClick(false);
+        }
+    }
+
+    return (
+      <div className="border py-2 px-4 rounder flex flex-col items-center mb-2">
+        <img className="w-1/6" src={props.product.image} />
+        <p className="font-bold">{props.product.title}</p>
+        <p className="">{props.product.price}</p>
+        {btnClick && <p>{props.product.description}</p>}
+        
+        <button 
+        className={!btnClick ? "py-2 px-4 border bg-yellow-400" : "py-2 px-4 border bg-blue-400"} 
+        onClick={() => click()}
+        >{!btnClick ? 'Show more' : 'Hide details'}
+        </button>
+      </div>
+    )
+}
+export default Product;
